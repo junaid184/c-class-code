3e60106c534ae39c8d8b139482ba9f41070517d0
Junaid Shakeel Ahmed <79161332+junaid184@users.noreply.github.com>
2021-11-09 13:52:42 +0500

    Add files via upload

--- Binary search.cpp
+++ Binary search.cpp
@@ -1,42 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-	int c,first,last,mid,n,search,arr[100];
-	cout<<"\nEnter numbers of Elements ";
-	cin>>n;
-	cout<<"Enter "<<n<<"elements";
-	for(c=0;c<n;c++)
-	{
-		cin>>arr[c];
-	}
-	cout<<"Enter value to search";
-	cin>>search;
-	first=0;
-	last=n-1;
-	mid=(last+first)/2;
-	while(first<=last)
-	{
-		if(arr[mid]<search)
-		{
-			first=mid+1;
-		}
-		else if(arr[mid]==search)
-		{
-			cout<<"Found at Position"<<mid+1;
-			break;
-		}
-		else
-		{
-			last=mid-1;
-		}
-		/*if(first>last)
-		{
-			cout<<"not found";
-		}*/
-		
-	}
-
-	
-	return 0;
+    int i, arr[100], num, first, last, middle;
+    cout<<"Enter 10 Elements (in ascending order): ";
+    for(i=0; i<10; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to be Search: ";
+    cin>>num;
+    first = 0;
+    last = 9;
+    middle = (first+last)/2;
+    while(first <= last)
+    {
+        if(arr[middle]<num)
+            first = middle+1;
+        else if(arr[middle]==num)
+        {
+            cout<<"\nThe number, "<<num<<" found at Position "<<middle+1;
+            break;
+        }
+        else
+            last = middle-1;
+        middle = (first+last)/2;
+    }
+    if(first>last)
+        cout<<"\nThe number, "<<num<<" is not found in given Array";
+    cout<<endl;
+    return 0;
 }
